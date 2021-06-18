b6424a6f8a712631e7d18204af3b8c95a41c6394
Bruno Mota <brunomottta@terra.com.br>
2021-06-18 12:13:49 -0300

    tratamento de erros em REST - transactions

--- src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -48,7 +49,14 @@ public class StudentEndpoint {
 	}
 	
 	@PostMapping
+	@Transactional(rollbackFor = Exception.class) // Para tratar exception Checked Ou Retirar o rollback para excepetion Unchecked
 	public ResponseEntity<?> save(@RequestBody Student student){
+		studentDAO.save(student);
+		student.setId(null);
+		studentDAO.save(student);
+		if(true)
+			throw new RuntimeException("Test Transaction");
+		studentDAO.save(student);
 		return new ResponseEntity<>(studentDAO.save(student), HttpStatus.CREATED);
 	}
 	
